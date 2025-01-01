234355749d7d4408064debc96192eab89c45c766
Mohammad Aman <64080869+mohammadaman055@users.noreply.github.com>
2025-01-01 12:11:52 +0530

    Time: 2 ms (24.89%), Space: 8.7 MB (52.6%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int left=0;
+        int right=1;
+        int max=0;
+        while(left<right && right<s.length()){
+            int leftcount=0;
+            for(int j=0;j<=left;j++){
+                if(s[j]=='0'){
+                    leftcount++;
+                }
+            }
+
+            int rightcount=0;
+            for(int k=right;k<s.length();k++){
+                if(s[k]=='1'){
+                    rightcount++;
+                }
+            }
+            
+            if(max<(leftcount+rightcount) ){
+                max=leftcount+rightcount;
+            }
+
+            left++;
+            right++;
+        }
+
+        return max;
+    }
+};
